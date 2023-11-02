08405e6b85b4dd560ad713fa9f4334890bde5985
JatinKevlani <jatinkevlani01@gmail.com>
2023-11-02 19:36:07 +0530

    progress

--- dashboard.php
+++ dashboard.php
@@ -1,365 +1,71 @@
 <?php
-  session_start();
-  if (isset($_SESSION['user'])) {
-      $user = $_SESSION['user'];
-  } else {
-      $user = array();
-  }
-  if ($_POST) {
-    var_dump($_POST);
-    $name = $_POST['name'];
-    $position = $_POST['position'];
-    $office = $_POST['office'];
-    $age = $_POST['age'];
-    $startdate = $_POST['startdate'];
-    $salary = $_POST['salary'];
-    echo "<br>" . $name . "<br>" . $position . "<br>" . $office . "<br>" . $age . "<br>" . $startdate . "<br>" . $salary;
-  }
+    session_start();
+    if (isset($_SESSION['user'])) {
+        $user = $_SESSION['user'];
+    } else {
+        $user = array();
+    }
+    if(!isset($user[1])){
+        echo "<h1>Please <a href='login.php'>Login</a> first</h1>";
+    }
+    else{
+        include('database/connection.php');
+        $database_name = 'inventory';
+        if (!mysqli_select_db($conn, $database_name)) {
+            die("Database selection failed: " . mysqli_error($conn));
+        }
+        $query = "SELECT * FROM products";
+        $result = $conn->query($query);
+        if (!$result) {
+            die("Query failed: " . mysqli_error($conn));
+        }
+        $data = $result->fetch_all();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
-  <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css" />
-  <link rel="stylesheet" href="css/style.css" />
-  <title>Admin Panel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
 </head>
-
 <body>
-  <!-- top navigation bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-      <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#sidebar" aria-controls="offcanvasExample">
-        <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
-      </button>
-      <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="#">Inventory management System</a>
-      <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="collapse"
-        data-bs-target="#topNavBar" aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="topNavBar">
-        <form class="d-flex ms-auto my-3 my-lg-0">
-          <div class="input-group">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-primary" type="submit">
-              <i class="bi bi-search"></i>
-            </button>
-          </div>
-        </form>
-        <p id="username-text" class="text-white" style="padding-top: 13px; padding-left: 10px;">
-          ID : <?= $user[0]?> <br>
-          Hello, <?= $user[1]?> <br>
-          Email : <?= $user[2]?> <br>
-          Password : <?= $user[3]?> <br>
-          Created at : <?= $user[4]?> <br>
-          Updated at : <?= $user[5]?> <br>
+    <?php if(isset($user[1])) { ?>
+        <p>
+            <h1>Hello, <?= $user[1]?></h1> <br>
+            User ID : <?= $user[0]?> <br>
+            Email : <?= $user[2]?> <br>
+            Password : <?= $user[3]?> <br>
+            Created at : <?= $user[4]?> <br>
+            Updated at : <?= $user[5]?> <br>
         </p>
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              <i class="bi bi-person-fill"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- top navigation bar -->
-  <!-- offcanvas -->
-  <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
-    <div class="offcanvas-body p-0">
-      <nav class="navbar-dark">
-        <ul class="navbar-nav">
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3">
-              CORE
-            </div>
-          </li>
-          <li>
-            <a href="#" class="nav-link px-3 active">
-              <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-              <span>Dashboard</span>
-            </a>
-          </li>
-          <li class="my-4">
-            <hr class="dropdown-divider bg-light" />
-          </li>
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
-              Interface
-            </div>
-          </li>
-          <li>
-            <a class="nav-link px-3 sidebar-link" data-bs-toggle="collapse" href="#layouts">
-              <span class="me-2"><i class="bi bi-layout-split"></i></span>
-              <span>Layouts</span>
-              <span class="ms-auto">
-                <span class="right-icon">
-                  <i class="bi bi-chevron-down"></i>
-                </span>
-              </span>
-            </a>
-            <div class="collapse" id="layouts">
-              <ul class="navbar-nav ps-3">
-                <li>
-                  <a href="#" class="nav-link px-3">
-                    <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-                    <span>Dashboard</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="#" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-book-fill"></i></span>
-              <span>Pages</span>
-            </a>
-          </li>
-          <li class="my-4">
-            <hr class="dropdown-divider bg-light" />
-          </li>
-          <li>
-            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
-              Addons
-            </div>
-          </li>
-          <li>
-            <a href="#" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-graph-up"></i></span>
-              <span>Charts</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link px-3">
-              <span class="me-2"><i class="bi bi-table"></i></span>
-              <span>Tables</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <!-- offcanvas -->
-  <main class="mt-5 pt-3">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <h4 style="padding-top: 15px;">Dashboard</h4>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-3 mb-3">
-          <div class="card bg-primary text-white h-100">
-            <div class="card-body py-5">Total Sales</div>
-            <div class="card-footer d-flex">
-              View Details
-              <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <div class="card bg-warning text-dark h-100">
-            <div class="card-body py-5">Turnover</div>
-            <div class="card-footer d-flex">
-              View Details
-              <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <div class="card bg-success text-white h-100">
-            <div class="card-body py-5">Total employees</div>
-            <div class="card-footer d-flex">
-              View Details
-              <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 mb-3">
-          <div class="card bg-danger text-white h-100">
-            <div class="card-body py-5">User requests</div>
-            <div class="card-footer d-flex">
-              View Details
-              <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 offset-md-2 mb-3">
-          <div class="card h-100">
-            <div class="card-header">
-              <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-              Sales in Last week
-            </div>
-            <div class="card-body">
-              <canvas class="chart" width="400" height="200"></canvas>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 mb-3">
-          <div class="card">
-            <div class="card-header">
-              <span><i class="bi bi-plus"></i></span> Add new data
-            </div>
-            <div class="card-body">
-              <form action="admin.php" method="post" class="container">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="name">Name:</label>
-                      <input type="text" id="name" name="name" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="position">Position:</label>
-                      <input type="text" id="position" name="position" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="office">Office:</label>
-                      <input type="text" id="office" name="office" class="form-control" required>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="age">Age:</label>
-                      <input type="number" id="age" name="age" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="startdate">Start Date:</label>
-                      <input type="date" id="startdate" name="startdate" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="salary">Salary:</label>
-                      <input type="number" id="salary" name="salary" class="form-control" required>
-                    </div>
-                  </div>
-                </div>
-                <input type="submit" value="Submit" class="btn btn-primary " style="margin-top: 15px;">
-              </form>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div class="row">
-        <div class="col-md-12 mb-3">
-          <div class="card">
-            <div class="card-header">
-              <span><i class="bi bi-table me-2"></i></span> Data Table
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table id="example" class="table table-striped data-table" style="width: 100%">
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
-                      <th>Salary</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Tiger Nixon</td>
-                      <td>System Architect</td>
-                      <td>Edinburgh</td>
-                      <td>61</td>
-                      <td>2011/04/25</td>
-                      <td>$320,800</td>
-                    </tr>
-                    <tr>
-                      <td>Garrett Winters</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>63</td>
-                      <td>2011/07/25</td>
-                      <td>$170,750</td>
-                    </tr>
-                    <tr>
-                      <td>Ashton Cox</td>
-                      <td>Junior Technical Author</td>
-                      <td>San Francisco</td>
-                      <td>66</td>
-                      <td>2009/01/12</td>
-                      <td>$86,000</td>
-                    </tr>
-                    <tr>
-                      <td>Cedric Kelly</td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td>$433,060</td>
-                    </tr>
-                    <tr>
-                      <td>Shad Decker</td>
-                      <td>Regional Director</td>
-                      <td>Edinburgh</td>
-                      <td>51</td>
-                      <td>2008/11/13</td>
-                      <td>$183,000</td>
-                    </tr>
-                    <tr>
-                      <td>Michael Bruce</td>
-                      <td>Javascript Developer</td>
-                      <td>Singapore</td>
-                      <td>29</td>
-                      <td>2011/06/27</td>
-                      <td>$183,000</td>
-                    </tr>
-                    <tr>
-                      <td>Donna Snider</td>
-                      <td>Customer Support</td>
-                      <td>New York</td>
-                      <td>27</td>
-                      <td>2011/01/25</td>
-                      <td>$112,000</td>
-                    </tr>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>Name</th>
-                      <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
-                      <th>Salary</th>
-                    </tr>
-                  </tfoot>
+        <a href="logout.php">Logout</a>
+        <?php if(isset($data)) { ?>
+            <div class="products">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Prod ID</th>
+                            <th>Prod Category</th>
+                            <th>Prod Brand</th>
+                            <th>Prod Name</th>
+                            <th>Prod Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data as $product) { ?>
+                            <tr>
+                                <td><?= $product[0] ?></td>
+                                <td><?= $product[1] ?></td>
+                                <td><?= $product[2] ?></td>
+                                <td><?= $product[3] ?></td>
+                                <td><?= $product[4] ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
                 </table>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-  <script src="./js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
-  <script src="./js/jquery-3.5.1.js"></script>
-  <script src="./js/jquery.dataTables.min.js"></script>
-  <script src="./js/dataTables.bootstrap5.min.js"></script>
-  <script src="./js/script.js"></script>
+        <?php } ?>
+    <?php } ?>
 </body>
-
 </html>
